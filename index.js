8d1ab1f61def56ea26f573c222c13cf8edcb9140
Shawn Witemyre <switemyre@comcast.net>
2025-03-12 18:04:17 -0400

    Added tab button that gets url and save it in localStorage Added manifast.json

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 function render(leads) {
     let listItems = ""
@@ -34,5 +43,5 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render()
+    render(myLeads)
 })
